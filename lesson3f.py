24e0daad494432cfd60081f780d3ec111ad5519b
brightlord001 <pablomungah@gmail.com>
2026-02-09 16:29:32 +0300

    friday

--- lesson3f.py
+++ lesson3f.py
@@ -1,4 +1,18 @@
 # create a python that is able to determine whether a umber enterd is an even nember or odd 
 number = int(input("Enter number: "))
+if number%2 == 0 :
+   print("The number is even")
 
-if number % 2 == 0
+else :
+   print("The number is odd")
+
+# Create a python program that is able to determine whether a person can donate blood based on age and weight of a person . if the weight is greater than 50kg and age is above 18, then the person can donate blood else cannot donate 
+
+weight = float(input("Enter your weight here: "))
+age = int(input("Enter yor age: "))
+
+if age >=18 and weight >50 :
+   print("You can donate blood")
+
+else :
+   print("You cannot donate blood") 
